c25dd458ae8aef4572877809a83849812921ddda
Paul Gallagher <gallagher.paul@gmail.com>
2011-10-14 23:25:38 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Navd::Application.config.secret_token = '842b9ea5e9ef155e8195bd6079e6817d1ec2b5c44ec878ad85fcf2fccac427db86aa0066e5f6afd7cb1e0cf79771e0a242d90000d79a213502ceb6af37f2196f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Navd::Application.config.session_store :cookie_store, :key => '_navd_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Navd::Application.config.session_store :active_record_store
