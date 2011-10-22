e080bc2ea23acb06f47442edfb83efa1d99da41b
Paul Gallagher <gallagher.paul@gmail.com>
2011-10-22 19:40:24 +0800

    add google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,4 +51,20 @@ src="http://www.google.com/reader/ui/3523697345-audio-player.swf?audioUrl=#{sour
   rescue
     Show.latest
   end
+
+  def insert_ga_script(ga_id)
+    return unless ga_id
+    return raw <<-EOGA
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '#{ga_id}']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+    EOGA
+  end
 end
